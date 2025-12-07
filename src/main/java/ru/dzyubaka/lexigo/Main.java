b2a86e7d52c84115aa1c8d880160cf027ee70763
Konstantin Dzyuba <dzyubaka3@gmail.com>
2025-12-07 13:46:11 +0300

    FileChooser -> ChoiceDialog

--- src/main/java/ru/dzyubaka/lexigo/Main.java
+++ src/main/java/ru/dzyubaka/lexigo/Main.java
@@ -3,18 +3,12 @@ package ru.dzyubaka.lexigo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
-    public static final FileChooser fileChooser = new FileChooser();
-
     public static void main(String[] args) {
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV", "*.csv"));
         Application.launch(args);
     }
 
