3d4262ba736e26f9804d5818576479f19cdf67cf
Konstantin Dzyuba <dzyubaka3@gmail.com>
2025-12-10 11:14:42 +0300

    Hints

--- src/main/java/ru/dzyubaka/lexigo/Main.java
+++ src/main/java/ru/dzyubaka/lexigo/Main.java
@@ -8,16 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) throws IOException {
-        var scene = new Scene(FXMLLoader.load(Main.class.getResource("view/menu.fxml")), 640, 360);
+        var scene = new Scene(FXMLLoader.load(Main.class.getResource("view/menu.fxml")), 640, 400);
         scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setTitle("LexiGo! v0.3.0");
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
