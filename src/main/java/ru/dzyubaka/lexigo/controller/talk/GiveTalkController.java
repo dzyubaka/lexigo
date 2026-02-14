99c1a8f57ab72fa6b3dca42ff18ee3e910033c22
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-15 02:11:42 +0300

    Add give image loading

--- src/main/java/ru/dzyubaka/lexigo/controller/talk/GiveTalkController.java
+++ src/main/java/ru/dzyubaka/lexigo/controller/talk/GiveTalkController.java
@@ -3,6 +3,7 @@ package ru.dzyubaka.lexigo.controller.talk;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -10,17 +11,25 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.util.Duration;
 import ru.dzyubaka.lexigo.controller.MenuController;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class GiveTalkController {
+    @FXML
+    private BorderPane borderPane;
+
     @FXML
     private ToolBar toolBar;
 
@@ -29,6 +38,9 @@ public class GiveTalkController {
 
     @FXML
     private TextFlow textFlow;
+
+    @FXML
+    private ImageView imageView;
 
     private Timeline timeline;
 
@@ -88,6 +100,8 @@ public class GiveTalkController {
                     new Text("\n\n"),
                     new Text(lines[2])
             );
+            imageView.setImage(SwingFXUtils.toFXImage(ImageIO.read(new File(name + ".jpg")), null));
+            imageView.fitHeightProperty().bind(borderPane.heightProperty().divide(2));
             millisLeft = prepareMillis;
         } catch (IOException e) {
             throw new UncheckedIOException(e);
